c184927802a40a75ae223a3a3494827d08ae4df3
Yasushi Abe <yasushi.abe@gmail.com>
2015-01-03 21:20:26 +0900

    multimethod

--- src/simple/core.clj
+++ src/simple/core.clj
@@ -1,47 +1,40 @@
 (ns simple.core)
 
-(defprotocol Base
-  (inspect [this]))
-(defprotocol Reducible
-  (reducible? [this])
-  (sreduce [this]))
+(defmulti s-inspect "inspect expression" class)
+(defmethod s-inspect :default [this] (str "«" (.toString this) "»"))
+
+(defmulti s-reducible? "" class)
+(defmethod s-reducible? :default [this] false)
+
+(defmulti s-reduce "" class)
 
 (defrecord SNumber [value]
   Object
-  (toString [_] (str value))
-  Base
-  (inspect [this] (str "«" (.toString this) "»"))
-  Reducible
-  (reducible? [_] false)
-  (sreduce [this] this))
+  (toString [_] (str value)))
+(defmethod s-reducible? SNumber [_] false)
+(defmethod s-reduce SNumber [this] this)
 
-(defrecord Add [left right]
+(defrecord SAdd [left right]
   Object
-  (toString [_] (str (.toString left) " + " (.toString right)))
-  Base
-  (inspect [this] (str "«" (.toString this) "»"))
-  Reducible
-  (reducible? [_] true)
-  (sreduce [_]
-    (cond
-      (reducible? left) (->Add (sreduce left) right)
-      (reducible? right) (->Add left (sreduce right))
-      :else (->SNumber (+ (.value left) (.value right))))))
+  (toString [_] (str (.toString left) " + " (.toString right))))
+(defmethod s-reducible? SAdd [_] true)
+(defmethod s-reduce SAdd [{:keys [left right]}]
+  (cond
+    (s-reducible? left) (->SAdd (s-reduce left) right)
+    (s-reducible? right) (->SAdd left (s-reduce right))
+    :else (->SNumber (+ (.value left) (.value right)))))
 
-(defrecord Multiply [left right]
+(defrecord SMultiply [left right]
   Object
-  (toString [_] (str (.toString left) " * " (.toString right)))
-  Base
-  (inspect [this] (str "«" (.toString this) "»"))
-  Reducible
-  (reducible? [_] true)
-  (sreduce [_]
-    (cond
-      (reducible? left) (->Add (sreduce left) right)
-      (reducible? right) (->Add left (sreduce right))
-      :else (->SNumber (* (.value left) (.value right))))))
+  (toString [_] (str (.toString left) " * " (.toString right))))
+(defmethod s-reducible? SMultiply [_] true)
+(defmethod s-reduce SMultiply [{:keys [left right]}]
+  (cond
+    (s-reducible? left) (->SMultiply (s-reduce left) right)
+    (s-reducible? right) (->SMultiply left (s-reduce right))
+    :else (->SNumber (* (.value left) (.value right)))))
 
-(defn run [expression]
+(defn s-run [expression]
   (cons expression
-        (lazy-seq (map sreduce
-                       (take-while reducible? (iterate sreduce expression))))))
+        (lazy-seq (map s-reduce
+                       (take-while s-reducible? (iterate s-reduce expression))))))
